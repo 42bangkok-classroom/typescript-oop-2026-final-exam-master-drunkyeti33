766baf8755c3176def2a367c98749dd8ea090083
Wiphusan Choonpicharn <itamcomemaiback@gmail.com>
2026-03-25 10:40:43 +0700

    s

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ApiResponse } from './interfaces/response.interface';
-const hello: ApiResponse[] = [
-  {success: boolean};
-  {data:
+const hello: ApiResponse = {
+  success: true,
+  data:
     service: purchase-api, 
     version: 1.0.0
-  };
-  {message: 'Hello NestJS'};
-];
+  ,
+  message: "Hello NestJS"}
+;
 @Injectable()
 export class AppService {
   getHello(): string {
